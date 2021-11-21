a15dbe67946c3ba932aa1460119800fa337df332
Yousof Mersal <yousofmersal@gmail.com>
2021-11-21 18:18:48 +0100

    clean xmonad config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -227,18 +227,18 @@ myKeys conf@XConfig {XMonad.modMask = modm} = M.fromList $
 ------------------------------------------------------------------------
 -- Mouse bindings: default actions bound to mouse events
 --
-myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
+myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList
 
     -- mod-button1, Set the window to floating mode and move by dragging
-    [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w
-                                       >> windows W.shiftMaster))
+    [ ((modm, button1), \w -> focus w >> mouseMoveWindow w
+                                       >> windows W.shiftMaster)
 
     -- mod-button2, Raise the window to the top of the stack
-    , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
+    , ((modm, button2), \w -> focus w >> windows W.shiftMaster)
 
     -- mod-button3, Set the window to floating mode and resize by dragging
-    , ((modm, button3), (\w -> focus w >> mouseResizeWindow w
-                                       >> windows W.shiftMaster))
+    , ((modm, button3), \w -> focus w >> mouseResizeWindow w
+                                       >> windows W.shiftMaster)
 
     -- you may also bind events to the mouse scroll wheel (button4 and button5)
     ]
@@ -349,7 +349,6 @@ main = do
 -- use the defaults defined in xmonad/XMonad/Config.hs
 --
 -- No need to modify this.
---
 defaults = ewmhFullscreen $ ewmh $ docks def {
       -- simple stuff
         terminal           = myTerminal,
