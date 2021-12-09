08abd57b6569fbf5e873b1d385dd80931eadef78
Yousof Mersal <yousofmersal@gmail.com>
2021-12-09 12:08:03 +0100

    update picom to use vsync

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -325,7 +325,7 @@ myLogHook = return ()
 myStartupHook = do
     spawnOnce "wired &"
     spawnOnce "nitrogen --restore &"
-    spawnOnce "picom &"
+    spawnOnce "picom --config ~/.config/picom/picom.conf &"
     --spawnOnce "trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 1 --transparent true --alpha 0 --tint 0x282c34  --height 22 &"
     --return kdeConfig
 
